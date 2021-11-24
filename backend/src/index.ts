50fc4b71120b4ce1c0a6733fbaac6651637decf9
franciszek marciniak <prostyfranek@protonmail.com>
2021-11-24 20:45:40 +0100

    deploy checkpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import passport from 'passport';
 import session from 'express-session';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import passportConfig from './utils/passport-config';
 import authRouter from './routes/auth';
 import groupsRouter from './routes/groups';
@@ -16,8 +17,9 @@ import { Lesson } from './models/LessonModel';
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+dotenv.config();
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 console.log(User, Group, Course, Grade, Lesson);
 app.use(
   cors({
@@ -29,12 +31,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: 'secret',
+    secret: process.env.SECRET_KEY || 'secret',
     resave: true,
     saveUninitialized: true,
   }),
 );
-app.use(cookieParser('secret'));
+app.use(cookieParser(process.env.SECRET_KEY || 'secret'));
 app.use(passport.initialize());
 app.use(passport.session());
 
--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -1,11 +1,33 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+const environment = process.env.NODE_ENV || 'development';
 
-const sequelize = new Sequelize('testdb', 'postgres', 'example', {
-  host: 'localhost',
-  dialect: 'postgres',
-  port: 5431,
-  logging: false,
-  omitNull: true,
-});
+dotenv.config();
+const config = {
+  db_name: process.env.DB_NAME || 'testdb',
+  db_username: process.env.DB_USERNAME || 'postgres',
+  db_password: process.env.DB_PASSWORD || 'example',
+  db_host: process.env.DB_HOST || 'localhost',
+  db_port: Number(process.env.DB_PORT) || 5431,
+};
+let sequelize: any;
+if (environment === 'development') {
+  sequelize = new Sequelize(
+    config.db_name,
+    config.db_username,
+    config.db_password,
+    {
+      host: config.db_host,
+      dialect: 'postgres',
+      port: config.db_port,
+      logging: false,
+      omitNull: true,
+    },
+  );
+} else if (environment === 'production') {
+  sequelize = new Sequelize(
+    'postgres://xrkaxxrvaeuulj:6befeb32a81f828169ae54137db08852caa94e27cd82992cd16d6b7c768bf5b3@ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/d87to7r7oucno6',
+  );
+}
 
 export default sequelize;
