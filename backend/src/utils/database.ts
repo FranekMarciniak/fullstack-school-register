8612f27516d03f814c7c66ed7ebc11d655cc5c94
franciszek marciniak <prostyfranek@protonmail.com>
2021-11-24 21:50:19 +0100

    deploy working

--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -28,6 +28,8 @@ if (environment === 'development') {
   sequelize = new Sequelize(process.env.DATABASE_URL || '', {
     dialect: 'postgres',
     ssl: true,
+    omitNull: true,
+    logging: false,
     dialectOptions: {
       ssl: {
         require: true,
