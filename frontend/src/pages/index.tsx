25cde7bb6e18b2cdf520450a71047891bbaadded
franciszek <prostyfranek@protonmail.com>
2022-02-01 16:45:38 +0100

    fixing typo

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -9,12 +9,11 @@ const Index = () => {
     <Main meta={<Meta title="Mars" description="" />}>
       <div className="p-5">
         <h1 className="text-xl my-4 font-medium text-center">
-          Mars school register is still in demo, fell free to look around and
+          Mars school register is still in demo, feel free to look around and
           login to any existing account.
         </h1>
         <p className="text-center my-4">
-          {" "}
-          Down below is a waltrough video shawcasing all the features
+          Down below is a walkthrough video showcasing all the features{" "}
         </p>
         <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
           <iframe
